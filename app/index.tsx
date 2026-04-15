81351ac402f46675316b428d408e9bfcb9aad447
shinziet-gh <chenshinzie@gmail.com>
2026-04-15 14:36:53 +1200

    use props to pass selected category and update news content

--- app/index.tsx
+++ app/index.tsx
@@ -2,27 +2,22 @@ import React, { useState } from 'react'
 import { View, ScrollView } from "react-native";
 import NavigationBar from './components/NavBar';
 import SocialMedia from './components/SocialMedia';
-import TV from './components/Favourites';
-import NewsFeed from './components/NewsFeed';
+import NewsPage from './components/NewsPage';
 
 export default function Index() {
 
-  const [media, setMedia] = useState(<NewsFeed/>);
-  const handleClick = (mode: string) => {
+  const [page, setPage] = useState("");
+  const handleClick = (category: string) => {
+    setPage(category === 'top-headlines' ? "" : category);
 
-    if (mode === 'newspage') {
-      setMedia(<NewsFeed/>);
-    }
-    else if (mode === 'favpage') {
-      setMedia(<TV/>);
-    }
+    console.log('Selected category:', page);
 }
 
   return (
     <View style={{ flex: 1 }}>
       <NavigationBar handleClick={handleClick}/>
       <ScrollView>
-        {media}
+        <NewsPage category={page}/>
       </ScrollView>
       <SocialMedia />
     </View>
