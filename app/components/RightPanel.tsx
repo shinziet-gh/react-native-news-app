edd720a8cc179206fc9fa88c4ebcad4c6c51206d
shinziet-gh <chenshinzie@gmail.com>
2026-04-21 12:22:07 +1200

    fix(calendar): enable marking on date select and reset on new selection of date range

--- app/components/RightPanel.tsx
+++ app/components/RightPanel.tsx
@@ -10,69 +10,85 @@ export default function RightPanel() {
     const minDate = new Date(dateNow);
     const maxDate = new Date(dateNow);
 
+    //Limit date selection to the last three months
     minDate.setMonth(minDate.getMonth() - 3);
     maxDate.setDate(maxDate.getDate() - 1);
 
-    const [dateValue, setDateValue] = useState<DateData>();
-    const [dateRangeBetween, setDateRangeBetween] = useState<DateData[]>([]);
-    const [dateRangeEnd, setDateRangeEnd] = useState<DateData>();
-    const [markingStyle, setMarkingStyle] = useState<Record<string, Object>>();
+    //Use state variables to store date info
+    const [startDate, setStartDate] = useState<DateData | null>(null);
+    const [endDate, setEndDate] = useState<DateData | null>(null);
+    const [datesInBetween, setDatesInBetween] = useState<DateData[]>([]);
+    const [markedDates, setMarkedDates] = useState<Record<string, Object>>({});
 
+    //CSS styles for markedDate property on react native calendar
     const markedDateStartStyle = { startingDay: true, color: 'black', textColor: 'white' };
     const markedDateStyle = { color: 'black', textColor: 'white' };
     const markedDateEndStyle = { endingDay: true, color: 'black', textColor: 'white' };
 
     const handleDateClick = (date: DateData) => {
         try {
-            console.log("Event clicked");
+            //Mark selected date on calendar, if startDate is initially empty
+            if (!startDate) {
+                setStartDate(date);
+                setMarkedDates({
+                    [date.dateString]: markedDateStartStyle,
+                });
+                return;
+            };
 
-            setMarkingStyle({});
-            setDateRangeBetween([]);
-
-            const tempStyles: Record<string, Object> = {};
-
-
-
-            if (!dateValue) return;
-
-            tempStyles[dateValue.dateString] = markedDateStartStyle;
-
-            setMarkingStyle(tempStyles);
-
-            const selected = new Date(date.dateString);
-            const current = new Date(dateValue.dateString);
-
-            if (selected > current) {
-
-                let tempDate = new Date(current);
-                while (tempDate < selected) {
-
-                    console.log("While loop");
-                    tempDate.setDate(tempDate.getDate() + 1);
-
-                    const newDateData: DateData = {
-                        year: tempDate.getFullYear(),
-                        month: tempDate.getMonth(),
-                        day: tempDate.getDay(),
-                        timestamp: tempDate.getTime(),
-                        dateString: tempDate.toISOString().split("T")[0],
-                    };
-
-                    dateRangeBetween.push(newDateData);
-                }
-                setDateRangeEnd(date);
-
-                //Do CSS binding
-
-                for (const tempDate of dateRangeBetween) {
-                    tempStyles[tempDate.dateString] = markedDateStyle;
-                }
-                tempStyles[date.dateString] = markedDateEndStyle;
-
-                setMarkingStyle(tempStyles);
-
+            //Reset state variable values to empty list or null
+            if (startDate && endDate) {
+                setStartDate(date);
+                setEndDate(null);
+                setMarkedDates({
+                    [date.dateString]: markedDateStartStyle,
+                });
+                setDatesInBetween([]);
+                return;
             }
 
+            if (startDate) {
+                //Store selected dates and their CSS styles
+                const tempStyles: Record<string, Object> = {};
+
+                //Temporary variables for iterating through while loop
+                const selected = new Date(date.dateString);
+                const current = new Date(startDate.dateString);
+
+                //Store dates between the starting date and selected date in array
+                if (selected > current) {
+                    let tempDate = new Date(current);
+                    let tempDatesInBetween: DateData[] = [];
+
+                    while (tempDate < selected) {
+                        console.log("temp date");
+                        tempDate.setDate(tempDate.getDate() + 1);
+
+                        //Create temporary DateData object
+                        const tempDateDate: DateData = {
+                            year: tempDate.getFullYear(),
+                            month: tempDate.getMonth(),
+                            day: tempDate.getDay(),
+                            timestamp: tempDate.getTime(),
+                            dateString: tempDate.toISOString().split("T")[0],
+                        };
+
+                        //Store DateData object to array
+                        tempDatesInBetween.push(tempDateDate);
+                    }
+
+                    //Do CSS binding to mark dates on calendar
+                    tempStyles[startDate.dateString] = markedDateStartStyle;
+                    for (const tempDate of tempDatesInBetween) {
+                        tempStyles[tempDate.dateString] = markedDateStyle;
+                    }
+                    tempStyles[date.dateString] = markedDateEndStyle;
+
+                    //Set state variables for selected dates
+                    setMarkedDates(tempStyles);
+                    setEndDate(date);
+                }
+            }
         } catch (error) {
             console.log(error)
         }
@@ -81,17 +97,13 @@ export default function RightPanel() {
 
     return (
         <Box>
-            <Text>{dateValue?.dateString}</Text>
-            <Text>{dateRangeEnd?.dateString}</Text>
-            <Text>{JSON.stringify(markingStyle)}</Text>
             <Calendar
                 markingType={'period'}
                 onDayPress={(date: DateData) => {
-                    setDateValue(date);
                     handleDateClick(date);
                 }}
                 maxDate={maxDate.toISOString()}
-                markedDates={markingStyle}
+                markedDates={markedDates}
                 disableAllTouchEventsForDisabledDays={true}
             />
         </Box>
