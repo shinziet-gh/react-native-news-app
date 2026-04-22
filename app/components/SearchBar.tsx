d5c684dab26fc62ca4a3aee4e281b700ff4d9bdc
shinziet-gh <chenshinzie@gmail.com>
2026-04-22 15:22:56 +1200

    feat(searchbar): use param props for placeholder and component width

--- app/components/SearchBar.tsx
+++ app/components/SearchBar.tsx
@@ -1,13 +1,13 @@
 import { Heading, Input, InputField, InputIcon, InputSlot, SearchIcon, VStack } from "@gluestack-ui/themed";
 
-export default function SearchBar() {
+export default function SearchBar({ placeholder, barWidth }: { placeholder: string, barWidth: string }) {
     return (
-        <VStack space="md" alignSelf="center">
-            <Input variant="outline" width="100%" borderRadius="10" py="$1" px="$2">
+        <VStack w={barWidth} space="md" alignSelf="center">
+            <Input variant="outline" borderRadius="10" py="$1" px="$2">
                 <InputSlot className="pl-3">
                     <InputIcon as={SearchIcon} />
                 </InputSlot>
-                <InputField placeholder="Search news..." />
+                <InputField placeholder={placeholder} />
             </Input>
         </VStack>
     )
