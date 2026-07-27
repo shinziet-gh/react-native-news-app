17106965d4154aca4ab77b5570eed0ab8ec5c5e5
shinziet-gh <chenshinzie@gmail.com>
2026-07-27 14:51:18 +1200

    change default category to 'top'

--- app/index.tsx
+++ app/index.tsx
@@ -14,7 +14,7 @@ export default function Index() {
   const { width, height, isMobile, isTablet, isDesktop } = useResponsive();
 
   //State variable to store the selected news category
-  const [params, setParams] = useState({ category: "home", searchQuery: "", fromDate: "", toDate: "" });
+  const [params, setParams] = useState({ category: "top", searchQuery: "", fromDate: "", toDate: "" });
 
   //Set category on tab click
   const handleTabClick = (category: string) => {
